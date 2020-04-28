487d3c6c0a5519106e897c0df0d2d389000a2fc5
limhepro <github2@chralpha.com>
2020-04-28 08:27:30 +0800

    perf: lint

--- scripts/lazyload/lib/process.js
+++ scripts/lazyload/lib/process.js
@@ -6,13 +6,13 @@ function lazyProcess(htmlContent)  {
     let loadingImage = this.config.lazyload.loadingImage || 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAABlBMVEXMzMyWlpYU2uzLAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNgAAAAAgAB9HFkpgAAAABJRU5ErkJggg==';
     return htmlContent.replace(/<img(.*?)src="(.*?)"(.*?)>/gi, function (str, p1, p2) {
         // might be duplicate
-        if(/data-srcset/gi.test(str)){
+        if (/data-srcset/gi.test(str)){
             return str;
         }
-        if(/src="data:image(.*?)/gi.test(str)) {
+        if (/src="data:image(.*?)/gi.test(str)) {
             return str;
         }
-        if(/no-lazy/gi.test(str)) {
+        if (/no-lazy/gi.test(str)) {
             return str;
         }
         return str.replace(p2, p2 + '" class="lazyload" ' + 'data-srcset="' + p2 + '" srcset="' + loadingImage);
